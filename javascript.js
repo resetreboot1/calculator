788eb59c3d512b82d6c1abc8381d386cc6b49152
resetreboot1 <206232704+resetreboot1@users.noreply.github.com>
2025-09-18 21:32:27 +0600

    Add a function to call operator functions and add buttons

--- javascript.js
+++ javascript.js
@@ -17,3 +17,21 @@ function divide(a, b) {
 let firstNumber;
 let secondNumber;
 let operator;
+
+function operate(num1, num2, operator) {
+  if (operator === "-") {
+    return subtract(num1, num2)
+  }
+
+  else if (operator === "+") {
+    return add(num1, num2)
+  }
+
+  else if (operator === "/") {
+    return divide(num1, num2)
+  }
+
+  else if (operator === "*") {
+    return multiply(num1, num2)
+  }
+}

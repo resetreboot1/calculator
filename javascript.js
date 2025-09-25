1d6a38e2dff91b4272ad256f25a795293e0517b3
resetreboot1 <206232704+resetreboot1@users.noreply.github.com>
2025-09-25 21:24:48 +0600

    Mske calculator functional without decimal and backspace

--- javascript.js
+++ javascript.js
@@ -1,32 +1,34 @@
 let topContainer = document.querySelector(".top-half-container")
 let bottomContainer = document.querySelector(".bottom-half-container")
 let display = document.querySelector(".display")
+let equalButton = document.querySelector("#equal-button")
+let clearButton = document.querySelector("#clear-button")
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 let operators = ["+", "-", "*", "/"]
 let tempArr = []
 let inputOperator = false;
-let stringSecondNumber;
+let displayDigitsArray;
 
 function add(a, b) {
- return a + b;
+ return (a + b);
 }
 
 function subtract(a, b) {
- return a - b;
+ return (a - b);
 }
 
 function multiply(a, b) {
- return a * b;
+ return (a * b);
 }
 
 function divide(a, b) {
- return a / b;
+ return (a / b).toFixed(2);
 }
 
 let firstNumber = "";
 let secondNumber = "";
-let operator;
+let operator = "";
 
 function operate(num1, num2, operator) {
   if (operator === "-") {
@@ -52,15 +54,25 @@ topContainer.addEventListener("click", (e) =>{
     tempArr.push(e.target.textContent)
     display.textContent = tempArr.join("");
     firstNumber = display.textContent;
+    displayDigitsArray = display.textContent.split("")
+    if (displayDigitsArray.length > 20){
+      display.textContent = "Error press clear";
+      inputOperator = "broken"
+    }
   }
 
   if (operators.includes(e.target.textContent) 
      && firstNumber !== undefined
-     && inputOperator === false){
+     && (inputOperator === false || inputOperator === "pending") ){
     display.textContent += ` ${e.target.textContent} `;
     inputOperator = true;
     tempArr = [];
     operator = e.target.textContent;
+    displayDigitsArray = display.textContent.split("")
+    if (displayDigitsArray.length > 20){
+      display.textContent = "Error press clear";
+      inputOperator = "broken"
+    }
   }
 
   if (numbers.includes(+e.target.textContent)
@@ -69,6 +81,37 @@ topContainer.addEventListener("click", (e) =>{
     display.textContent += tempArr.join("");
     secondNumber += tempArr;
     tempArr = [];
-
+    displayDigitsArray = display.textContent.split("")
+    if (displayDigitsArray.length > 20){
+      display.textContent = "Error press clear";
+      inputOperator = "broken"
+    }
   }
 })
+
+equalButton.addEventListener("click", (e) => {
+  if (firstNumber !== "" && secondNumber !== ""
+     && inputOperator === true){
+    display.textContent = operate(+firstNumber, +secondNumber, operator);
+    inputOperator = "pending";
+    firstNumber = display.textContent;
+    secondNumber = "";
+    displayDigitsArray = display.textContent.split("")
+    if (displayDigitsArray.length > 20){
+      display.textContent = "Error press clear";
+      inputOperator = "broken"
+    }
+  }
+})
+
+clearButton.addEventListener("click", (e) => {
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
+  inputOperator = false;
+  display.textContent = '';
+  tempArr = [];
+})
+
+
+
